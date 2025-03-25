eed9d895d83d3a56d54aa913bbd910faca4ab0c8
diegoefraingonzal <13064@portalcedi.edu.mx>
2025-03-25 09:00:07 -0600

    chanhges

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -15,7 +15,7 @@ import { Auth } from '@angular/fire/auth';
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class SignupPage implements OnInit {
+export class SignUpPage implements OnInit {
 
   email: string = '';
   password: string = '';
